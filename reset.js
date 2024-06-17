e40a8414c0458591afcfbe2049aae4640b746d5e
Christoffer Jonsson <schnavzer@protonmail.com>
2024-06-17 19:59:32 +0200

    reset js tied to alt html

---
+++ reset.js
@@ -0,0 +1,7 @@
+function resetForms() {
+    document.getElementById('treForm').reset();
+    document.getElementById('teliaForm').reset();
+    document.getElementById('tele2Form').reset();
+    document.getElementById('telenorForm').reset();
+    calculateTotals();
+}
