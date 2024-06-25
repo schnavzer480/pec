cac5e3f64a72e58fa4410f9af9037f469b4e105c
Christoffer Jonsson <schnavzer@protonmail.com>
2024-06-25 19:36:14 +0200

    clean code

--- reset.js
+++ reset.js
@@ -1,7 +1,8 @@
 function resetForms() {
-    document.getElementById('treForm').reset();
-    document.getElementById('teliaForm').reset();
-    document.getElementById('tele2Form').reset();
-    document.getElementById('telenorForm').reset();
+    document.getElementById("treForm").reset();
+    document.getElementById("teliaForm").reset();
+    document.getElementById("tele2Form").reset();
+    document.getElementById("telenorForm").reset();
+    //document.getElementById("world").value = " ";
     calculateTotals();
 }
