a6af04d55a74acc1e3157e87809c9338377e5e34
Christoffer Jonsson <schnavzer@protonmail.com>
2024-06-29 09:55:18 +0200

    reset form for world input fixed

--- reset.js
+++ reset.js
@@ -4,5 +4,10 @@ function resetForms() {
     document.getElementById("tele2Form").reset();
     document.getElementById("telenorForm").reset();
     //document.getElementById("world").value = " ";
+    let inputField = document.getElementById("world");
+        inputField.value = "";
+        inputField.placeholder = "3Världen";
+        inputField.style.borderColor = '';
+        inputField.classList.remove('valid', 'invalid');
     calculateTotals();
 }
