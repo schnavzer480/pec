9bb27fa7c57a7c9d77eb7be2db40940711ef48a9
Christoffer Jonsson <schnavzer@protonmail.com>
2024-08-27 19:37:00 +0200

    getelementbyid -> queryselector

--- onlynum.js
+++ onlynum.js
@@ -4,6 +4,6 @@ function allowOnlyNumbers(event) {
     input.value = input.value.replace(/[^0-9]/g, '');
 }
 
-document.getElementById('telia-extra').addEventListener('input', allowOnlyNumbers);
-document.getElementById('tele2-extra').addEventListener('input', allowOnlyNumbers);
-document.getElementById('telenor-extra').addEventListener('input', allowOnlyNumbers);
+document.querySelector('#telia-extra').addEventListener('input', allowOnlyNumbers);
+document.querySelector('#tele2-extra').addEventListener('input', allowOnlyNumbers);
+document.querySelector('#telenor-extra').addEventListener('input', allowOnlyNumbers);
