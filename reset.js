e7b83b5b33379958d98ed354e16969681cdacb2a
Christoffer Jonsson <schnavzer@protonmail.com>
2024-09-03 22:09:00 +0200

    cleaned code

--- reset.js
+++ reset.js
@@ -3,12 +3,11 @@ function resetForms() {
     document.querySelector("#teliaForm").reset();
     document.querySelector("#tele2Form").reset();
     document.querySelector("#telenorForm").reset();
-    //document.getElementById("world").value = " ";
+    
     let inputField = document.querySelector("#world");
         inputField.value = "";
         inputField.placeholder = "3Världen";
         inputField.style.borderColor = '';
-        //inputField.classList.remove('valid', 'invalid');
     calculateTotals();
 }
 
