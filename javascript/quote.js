7f19850b43d70316a5451cdf9422b054c342de44
Christoffer Jonsson <schnavzer@protonmail.com>
2024-09-29 10:23:21 +0200

    new quotes

--- javascript/quote.js
+++ javascript/quote.js
@@ -10,7 +10,8 @@ const quotes = [
     { text: "Vissa personer vill att saker ska ske, vissa hoppas att det ska ske, andra ser till att det sker.", author: "- Michael Jordan" },
     { text: "Du missar 100% av skotten du inte skjuter.", author: "- Wayne Gretzky" },
     { text: "Man ska inte göra sitt bästa. Man ska göra det som krävs.", author: "- Mikael Arndt" },
-    { text: "Hopp är ingen strategi.", author: "- August Walker (Mission Impossible VI" }
+    { text: "Hopp är ingen strategi.", author: "- August Walker (Mission Impossible VI)" },
+    { text: "Har du vaknat?! Mata på! Mata på! Mata på!", author: "- John Andreas" }
 ];
 
 let currentIndex = 0;
